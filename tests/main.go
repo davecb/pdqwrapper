ba4c1b2982344ee4c1f16c61733fda8843b6cace
David Collier-Brown <dave.collier-brown@indexexchange.com>
2024-11-14 14:42:56 -0500

    Do first few parameters

--- tests/main.go
+++ tests/main.go
@@ -1,125 +1,126 @@
 package main
 
-import "os"
-
-func main() {
-	os.Exit(0)
-}
-
-//
-//import (
-//	"fmt"
-//	"os"
-//	"path/filepath"
-//	"strconv"
-//)
-//
-//// PDQ constants (you'll need to implement or import these)
-//const (
-//	TERM  = 0
-//	CEN   = 1
-//	FCFS  = 2
-//	EXACT = 3
-//)
-//
-//var (
-//	nodes   int
-//	streams int
-//)
-//
-//func usage(progName string) {
-//	fmt.Fprintf(os.Stderr, "Usage: %s [-z think][-s service][-vd] from to by\n", progName)
-//}
+//import "os"
 //
 //func main() {
-//	var (
-//		from          = 1.0
-//		to            = 0.0
-//		by            = 0.0
-//		think         = 0.0
-//		serviceDemand = 0.0
-//		verbose       = false
-//		debug         = false
-//	)
-//
-//	progName := filepath.Base(os.Args[0])
-//
-//	if len(os.Args) == 1 {
-//		usage(progName)
-//		os.Exit(1)
-//	}
-//
-//	// Parse command line arguments
-//	i := 1
-//	for i < len(os.Args) && os.Args[i][0] == '-' {
-//		switch os.Args[i][1] {
-//		case 'h':
-//			usage(progName)
-//			os.Exit(0)
-//		case 'z', 't':
-//			i++
-//			think, _ = strconv.ParseFloat(os.Args[i], 64)
-//		case 'd', 'x':
-//			debug = true
-//		case 's':
-//			i++
-//			serviceDemand, _ = strconv.ParseFloat(os.Args[i], 64)
-//		case 'v':
-//			verbose = true
-//		default:
-//			fmt.Fprintf(os.Stderr, "%s: unknown option -%c, ignored.\n", progName, os.Args[i][1])
-//		}
-//		i++
-//	}
-//
-//	// Parse remaining arguments
-//	if i < len(os.Args) {
-//		from, _ = strconv.ParseFloat(os.Args[i], 64)
-//		i++
-//	}
-//	if i < len(os.Args) {
-//		to, _ = strconv.ParseFloat(os.Args[i], 64)
-//		i++
-//	}
-//	if i < len(os.Args) {
-//		by, _ = strconv.ParseFloat(os.Args[i], 64)
-//	}
-//	pdq(progName, think, serviceDemand, from, to, by, verbose, debug)
+//	os.Exit(0)
 //}
-//
-//func pdq(progName string, think, serviceDemand, from, to, by float64, verbose, debug bool) {
-//	// Check parameters
-//	if from < 0.0 {
-//		fmt.Fprintf(os.Stderr, "%s: from is negative, which is not defined. Halting.", progName)
-//		os.Exit(1)
-//	}
-//	if from == 0.0 {
-//		from = 1.0
-//	}
-//	if to <= 0.0 {
-//		to = from
-//	}
-//	if by <= 0.0 {
-//		by = 1.0
-//	}
-//
-//	//// Print headers
-//	//fmt.Printf("General closed solution from PDQ where serviceTime = %g think time = %g\n",
-//	//	serviceDemand, think)
-//	//
-//	//if verbose {
-//	//	fmt.Printf("Load\tThroughput\tUtilization\tQueueLen\tResidence\tResponse\n")
-//	//} else {
-//	//	fmt.Printf("\"# Load,\" Response\n")
-//	//}
-//
-//	for load := from; load <= to; load += by {
-//		doOneStep(load, think, serviceDemand, verbose)
-//	}
-//
-//}
-//
-//func doOneStep(load, think, serviceDemand float64, verbose bool) {
-//	fmt.Fprintf(os.Stderr, "load = %g think = %g "+
-//		"serviceTime = %g verbose = %t\n", load, think, serviceDemand, verbose)
-//}
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+// PDQ constants (you'll need to implement or import these)
+const (
+	TERM  = 0
+	CEN   = 1
+	FCFS  = 2
+	EXACT = 3
+)
+
+var (
+	nodes   int
+	streams int
+)
+
+func usage(progName string) {
+	fmt.Fprintf(os.Stderr, "Usage: %s [-z think][-s service][-vd] from to by\n", progName)
+}
+
+func main() {
+	var (
+		from          = 1
+		to            = 0
+		by            = 0
+		think         = 0.0
+		serviceDemand = 0.0
+		verbose       = false
+		debug         = false
+	)
+
+	progName := filepath.Base(os.Args[0])
+
+	if len(os.Args) == 1 {
+		usage(progName)
+		os.Exit(1)
+	}
+
+	// Parse command line arguments
+	i := 1
+	for i < len(os.Args) && os.Args[i][0] == '-' {
+		switch os.Args[i][1] {
+		case 'h':
+			usage(progName)
+			os.Exit(0)
+		case 'z', 't':
+			i++
+			think, _ = strconv.ParseFloat(os.Args[i], 64)
+		case 'd', 'x':
+			debug = true
+		case 's':
+			i++
+			serviceDemand, _ = strconv.ParseFloat(os.Args[i], 64)
+		case 'v':
+			verbose = true
+		default:
+			fmt.Fprintf(os.Stderr, "%s: unknown option -%c, ignored.\n", progName, os.Args[i][1])
+		}
+		i++
+	}
+
+	// Parse remaining arguments
+	if i < len(os.Args) {
+		from, _ = strconv.Atoi(os.Args[i])
+		i++
+	}
+	if i < len(os.Args) {
+		to, _ = strconv.Atoi(os.Args[i])
+		i++
+	}
+	if i < len(os.Args) {
+		by, _ = strconv.Atoi(os.Args[i])
+	}
+	Pdq(progName, think, serviceDemand, from, to, by, verbose, debug)
+}
+
+func Pdq(progName string, thinkTime, serviceTime float64, from, to, by int, verbose, debug bool) error {
+	// Check parameters
+	if from < 0 {
+		return fmt.Errorf("%s: \"from\" is negative, which is not defined. Halting.", progName)
+	}
+	if from == 0 {
+		// from iss only well-defined for positives, but choosing is a common, harmless user error
+		from = 1
+	}
+	if to <= 0 {
+		// this is bad code!   FIXME in the refactor
+		to = from
+	}
+	if by <= 0 {
+		// ditto
+		by = 1
+	}
+
+	//// Print headers
+	//fmt.Printf("General closed solution from PDQ where serviceTime = %g thinkTime time = %g\n",
+	//	serviceTime, thinkTime)
+	//
+	//if verbose {
+	//	fmt.Printf("Load\tThroughput\tUtilization\tQueueLen\tResidence\tResponse\n")
+	//} else {
+	//	fmt.Printf("\"# Load,\" Response\n")
+	//}
+
+	for load := from; load <= to; load += by {
+		doOneStep(load, thinkTime, serviceTime, verbose)
+	}
+	return nil
+}
+
+func doOneStep(load int, thinkTime, serviceTime float64, verbose bool) {
+	fmt.Fprintf(os.Stderr, "load = %d thinkTime = %g "+
+		"serviceTime = %g verbose = %t\n", load, thinkTime, serviceTime, verbose)
+}
--- tests/main_test.go
+++ tests/main_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"testing"
 )
 
+// func Pdq(progName string, think, serviceDemand, from, to, by float64, verbose, debug bool) {
 // t is positive number, upper bound unknown
 // z sleep is synonymous with t
 // s is service time, positibe number, upper bound unknown
@@ -14,22 +15,48 @@ import (
 // from is a positive number, defaulting to 1
 // to is a positive number, invalid if less than from
 // by is a positive number, smaller than to-from
-func Test_main(t *testing.T) {
+func Test_pdq(t *testing.T) {
 
 	type Sample struct {
-		Value float64
+		Value int64
 		Legal bool
 	}
 
 	positiveInt := []Sample{
-		Sample{
-			Value: 1.0,
-			Legal: true,
-		},
+		{-math.MaxInt64, false},
+		{-math.MaxInt64 + 1, false},
+		{-math.MaxInt64 / 2, false},
+		{-(math.MaxInt64 / 2) + 1, false},
+		{-3, false},
+		{-2, false},
+		{-1, false},
+		{0, false},
+		{1, true},
+		{2, true},
+		{3, true},
+		{4, true},
+		{(math.MaxInt64 / 2) - 1, true},
+		{(math.MaxInt64 / 2), true},
+		{math.MaxInt64 - 1, true},
+		{math.MaxInt64, true},
 	}
 
-	for i, sample := range positiveInt {
-		fmt.Printf("%d: Value = %g, Legal = %t\n",
-			i, sample.Value, sample.Legal)
+	for z, zStruct := range positiveInt {
+		// think time "t" is a positive number, upper bound unknown
+		// sleep time "z" is synonymous with t
+		//fmt.Printf("z%d: Value = %d, Legal = %t\n",
+		//	z, zStruct.Value, zStruct.Legal)
+		for s, sStruct := range positiveInt {
+			// s is service time, a positive number, upper bound unknown
+			//fmt.Printf("s%d: Value = %d, Legal = %t\n",
+			//	s, sStruct.Value, sStruct.Legal)
+			// inner test
+			err := Pdq("unit test", float64(zStruct.Value), float64(sStruct.Value), 1, 2, 1, false, false)
+			if err != nil {
+				t.Errorf("fail with z(%d) = %g, s(%d), %g\n",
+					z, float64(zStruct.Value), s, float64(sStruct.Value))
+			}
+
+		}
 	}
 }
